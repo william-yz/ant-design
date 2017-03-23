ad7bc3651e4ef2817435d8cd3a8db4c0843e611d
Wei Zhu <yesmeck@gmail.com>
2017-03-23 16:31:22 +0800

    Refactor RadioGroup's cloneElement to context

--- components/radio/radioButton.tsx
+++ components/radio/radioButton.tsx
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import Radio from './radio';
 
 export interface RadioButtonProps {
   value: string | number;
   style?: React.CSSProperties;
+  disabled?: boolean;
+  checked?: boolean;
+  onChange?: (e: any) => any;
 }
 
 export default class RadioButton extends React.Component<RadioButtonProps, any> {
-  static __ANT_RADIO_BUTTON = true;
-
   static defaultProps = {
     prefixCls: 'ant-radio-button',
   };
+
+  static contextTypes = {
+    group: PropTypes.any,
+  };
+
   render() {
+    let radioProps: RadioButtonProps = { ...this.props };
+    if (this.context.group) {
+      radioProps.onChange = this.context.group.onChange;
+      radioProps.checked = this.props.value === this.context.group.value;
+      radioProps.disabled = this.props.disabled || this.context.group.disabled;
+    }
+
     return (
-      <Radio {...this.props} />
+      <Radio {...radioProps} />
     );
   }
 }
